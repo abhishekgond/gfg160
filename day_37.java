e09b40cc818b94706c0f978152645e0a2aaf3362
Abhishek kumar <abhishekkumargond592@gmail.com>
2024-12-21 11:19:42 +0530

    Day_37

---
+++ day_37.java
@@ -0,0 +1,22 @@
+class Solution {
+  // Function to rotate matrix anticlockwise by 90 degrees.
+  static void rotateby90(int mat[][]) {
+    // code here
+    int n = mat.length;
+    for (int i = 0; i < n; i++) {
+      for (int j = i; j < n; j++) {
+        int temp = mat[i][j];
+        mat[i][j] = mat[j][i];
+        mat[j][i] = temp;
+      }
+    }
+    for (int i = 0; i < (n / 2); i++) {
+      for (int j = 0; j < n; j++) {
+        int temp = mat[i][j];
+        mat[i][j] = mat[n - 1 - i][j];
+        mat[n - 1 - i][j] = temp;
+      }
+    }
+
+  }
+}
