fd935a68a50c81797897d6e346267ad10a5a296e
Abhishek kumar <abhishekkumargond592@gmail.com>
2024-12-27 10:23:33 +0530

    Day_43

---
+++ day_43.java
@@ -0,0 +1,14 @@
+class Solution {
+  int countPairs(int arr[], int target) {
+    // Your code here
+    int count = 0;
+    HashMap<Integer, Integer> map = new HashMap<>();
+    for (int i : arr) {
+      if (map.containsKey(target - i)) {
+        count += map.get(target - i);
+      }
+      map.put(i, map.getOrDefault(i, 0) + 1);
+    }
+    return count;
+  }
+}
